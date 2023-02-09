1d7ebd24be217391fbcd1f4e919a33ccb78f4062
Mark Atherton <mark@insightinternet.co.uk>
2023-02-09 20:05:27 +0000

    added functionality so the yellow bar represents the progress of the video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -33,10 +33,16 @@ function handleRangeUpdate() {
   video[this.name] = [this.value];
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 /* Hook up Event Listener */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
