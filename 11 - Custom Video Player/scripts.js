59e6fe5beed095372fb060cb484d342144864732
Mark Atherton <mark@insightinternet.co.uk>
2023-02-09 19:44:20 +0000

    added volume and playback range functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -26,8 +26,11 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = [this.value];
 }
 
 /* Hook up Event Listener */
@@ -36,3 +39,4 @@ video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
