7b72ae37210c9993dd39b942c79964ea8252f5b1
Mark Atherton <mark@insightinternet.co.uk>
2023-02-09 20:32:10 +0000

    end of lesson: added scrub functionality so you can drag and drop the progressbar and video playback updates acordingly

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -51,4 +51,9 @@ video.addEventListener("timeupdate", handleProgress);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+
+let mousedown = false;
 progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
