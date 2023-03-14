6e557949813355684bac5a00a0654d961788ba5c
Mark Atherton <mark@insightinternet.co.uk>
2023-03-14 12:18:45 +0000

    added painToCanvas, takePhoto functions and added event listener to the player to run PaintToCanvas on page load and it can play

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -18,4 +18,24 @@ function getVideo() {
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  //played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of canvas
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
